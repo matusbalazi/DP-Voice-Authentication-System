6356c556cda72a12a8d8c3ba077a9d46a2dba278
Matúš <balazi@stud.uniza.sk>
2024-01-28 15:15:26 +0100

    Created About The Project Frame

--- gui/gui.py
+++ gui/gui.py
@@ -44,8 +44,49 @@ def button_open_door_callback():
     pass
 
 
+# create ABOUT FRAME widgets
 def button_about_project_callback():
-    pass
+    frame_intro.lower()
+    frame_about.lift()
+
+    label_main_title = ctk.CTkLabel(master=frame_about, text=Translations.get_translation('system_authentication'),
+                                    font=("Roboto", 48, "bold"), justify=ctk.CENTER)
+    label_main_title.grid(row=1, column=4, pady=10, padx=10, sticky="nsew")
+
+    button_exit = ctk.CTkButton(master=frame_about, text=Translations.get_translation('exit'),
+                                font=("Roboto", 32, "bold"),
+                                command=button_exit_callback)
+    button_exit.grid(row=7, column=1, pady=10, padx=10, sticky="nsew")
+
+    button_back = ctk.CTkButton(master=frame_about, text=Translations.get_translation('back'),
+                                font=("Roboto", 32, "bold"), command=button_back_callback)
+    button_back.grid(row=7, column=7, pady=10, padx=10, sticky="nsew")
+
+    label_thesis = ctk.CTkLabel(master=frame_about, text=Translations.get_translation('thesis'),
+                                font=("Roboto", 42, "bold"), justify=ctk.CENTER)
+    label_thesis.grid(row=2, column=4, pady=10, padx=10, sticky="nsew")
+
+    label_about_project = ctk.CTkLabel(master=frame_about,
+                                       text=Translations.get_translation('topic') + "\n" + Translations.get_translation(
+                                           'student') + "\n" + Translations.get_translation(
+                                           'mentor') + "\n" + Translations.get_translation('year'),
+                                       font=("Roboto", 32), justify=ctk.LEFT)
+    label_about_project.grid(row=3, column=4, pady=10, padx=10, sticky="nsew")
+
+    label_school = ctk.CTkLabel(master=frame_about, text=(
+            Translations.get_translation('university') + "\n\n" + Translations.get_translation(
+        'faculty') + "\n\n" + Translations.get_translation('department')), font=("Roboto", 32),
+                                justify=ctk.CENTER)
+    label_school.grid(row=5, column=4, pady=10, padx=10, sticky="nsew")
+
+    entry_password = ctk.CTkEntry(master=frame_about, font=("Roboto", 32, "bold"), placeholder_text="", show="*",
+                                  justify=ctk.CENTER)
+    entry_password.grid(row=6, column=4, pady=10, padx=10, sticky="nsew")
+
+    button_password = ctk.CTkButton(master=frame_about, text=Translations.get_translation('confirm'),
+                                    font=("Roboto", 32, "bold"),
+                                    command=lambda: button_password_callback(entry_password.get()))
+    button_password.grid(row=7, column=4, pady=10, padx=10, sticky="nsew")
 
 
 def button_exit_callback():
@@ -56,9 +97,25 @@ def segmented_button_language_callback(value):
     change_language(value)
 
 
+def button_back_callback():
+    frame_about.lower()
+    frame_intro.lift()
+
+
+def button_password_callback(value):
+    if (value == "0000"):
+        #frame_authentication_phrase_callback()
+        pass
+
+# create INTRO FRAME
 frame_intro = create_frame()
 frame_intro.lift()
 
+# create ABOUT FRAME
+frame_about = create_frame()
+frame_about.lower()
+
+# create INTRO FRAME widgets
 label_main_title = ctk.CTkLabel(master=frame_intro, text=Translations.get_translation('system_authentication'),
                                 font=("Roboto", 48, "bold"), justify=ctk.CENTER)
 label_main_title.grid(row=1, column=4, pady=10, padx=10, sticky="nsew")
